f793863a319ee158db35de3e6364c2f1946a25f9
Tal Maman <dreapet41s@gmail.com>
2025-01-23 12:28:46 +0200

    refactor: restructure app module and create shared and task modules for better organization

--- Angular/Angular-shared-components-modules/src/app/app.module.ts
+++ Angular/Angular-shared-components-modules/src/app/app.module.ts
@@ -5,14 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
-import { AddTaskComponent } from './tasks/add-task/add-task.component';
+import { SharedModule } from './shared/shared.module';
+import { TaskModule } from './tasks/tasks.module';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, TasksComponent, AddTaskComponent],
+    declarations: [AppComponent, HeaderComponent, UserComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, SharedModule, TaskModule],
 })
 
 export class AppModule {

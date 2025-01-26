af7c6e2d3693cec6153cd63a8bee2ec174b99f13
Tal Maman <dreapet41s@gmail.com>
2025-01-26 11:43:05 +0200

    feat: add ticket and new ticket components with basic structure and styling

--- Angular/Angular-Components-Deep-Dive/src/app/app.component.ts
+++ Angular/Angular-Components-Deep-Dive/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { DashboardSupportComponent } from "./dashboard/support/dashboard-support.component";
 import { DashboardServerComponent } from "./dashboard/server-status/dashboard-server.component";
 import { DashboardTrafficComponent } from "./dashboard/traffic/dashboard-traffic.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, DashboardSupportComponent, DashboardServerComponent, DashboardTrafficComponent],
+  imports: [HeaderComponent, DashboardSupportComponent, DashboardServerComponent, DashboardTrafficComponent, DashboardItemComponent],
 })
 export class AppComponent {
 }

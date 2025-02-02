0da0466f1cfd7bc810e983bc45878b9252b21c8b
Tal Maman <dreapet41s@gmail.com>
2025-02-02 16:48:05 +0200

    feat: add signup and login components with form handling, validation, and local storage integration

---
+++ Angular/Angular-Forms-Reactive/src/app/auth/signup/signup.component.ts
@@ -0,0 +1,94 @@
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    number: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.invalid &&
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty
+    );
+  }
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.invalid &&
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty
+    );
+  }
+
+  ngOnInit() {
+    const savedForm = window.localStorage.getItem('saved-signup-form');
+
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        email: loadedForm.email,
+      });
+    }
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+}
